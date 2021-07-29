a6581e15be77dbe75b658fa627fb450ecc6f9e54
Matt Riese <rise.matt@gmail.com>
2021-07-28 21:04:02 -0700

    new version of 3sum needs faster time complexity

--- leetcode-top-interview-qs-medium/threeSum.js
+++ leetcode-top-interview-qs-medium/threeSum.js
@@ -1,25 +1,34 @@
 "use strict";
-/**
+/** sort first
  * for each elem
+ *  // if this elem === the previous elem, then continue
  *  for each elem, starting at i+1
  *
  *    for each elem starting at j+1
- *      const newSum = []
+ *      // if this elem === the previous elem, then continue
  *      if nums[i] + nums[j] + nums[k] === 0
- *        then push those elements into newSum
+ *        then push those elements to newSum, and sort it.
+ *        then loop through answers and check if json.stringify === other answers
  *        and push newSum to answers
  */
 
+// TODO: this works but time complexity is too high for leetcode. Look into better algo
 function threeSum(nums) {
   const answers = [];
-  if (nums.length < 3) {
-    return answers;
-  }
   for (let i = 0; i < nums.length - 2; i++) {
     for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
+        let newTriplet = [];
+        let repeat = false;
         if (nums[i] + nums[j] + nums[k] === 0) {
-          answers.push([nums[i], nums[j], nums[k]])
+          newTriplet = [nums[i], nums[j], nums[k]].sort((a, b) => (a - b));
+          for (let answer of answers) {
+            if (JSON.stringify(newTriplet) === JSON.stringify(answer)) {
+              repeat = true;
+              break;
+            }
+          }
+        if (!repeat) answers.push(newTriplet);
         }
       }
     }
