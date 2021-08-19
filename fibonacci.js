fd9fdf78854428842e524c8f9e2ad310bf210e76
Matt Riese <rise.matt@gmail.com>
2021-08-19 12:08:41 -0700

    buggy and better dynamic prog fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,13 @@
-function fibo(n) {
+function fibo(n, answers={}) {
+  if (answers[n]) return answers[n];
   if (n === 1 || n === 2) return 1;
-  return fubi(n-1) + fibo(n-2);
+  answers[n] = fibo(n-1, answers) + fibo(n-2, answers);
+  return answers[n];
 }
+
+// function fibo(n, answers={}) {
+//   if (n === 1 || n === 2) return 1;
+//   answers[n-1] = fibo(n-1, answers);
+//   answers[n-2] = fibo(n-2, answers);
+//   return answers[n-1] + answers[n-2];
+// }
