3bef8637a444e42c0c67212eef081bf41f94fc47
Matt Riese <riese.matt@gmail.com>
2021-11-02 21:39:13 -0700

    added 121 best-day

---
+++ leetcode-top-interview-qs-medium/121-best-time-buy.test.js
@@ -0,0 +1,6 @@
+const { bestTime } = require('./121-best-time-buy');
+
+test('works with two leetcode examples', function () {
+  expect(bestTime([7, 1, 5, 3, 6, 4]).toEqual(5));
+  expect(bestTime([7, 6, 4, 3, 1]).toEqual(0));
+});
